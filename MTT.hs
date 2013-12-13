454a8eb97dd2b6827ce5138e1d20b0078bc981e0
Simon Huber <hubsim@gmail.com>
2013-12-13 12:19:40 +0100

    wip

--- MTT.hs
+++ MTT.hs
@@ -15,11 +15,14 @@ type Cont = [(String,Val)]
 
 type Def = ([(String,Exp)],[(String,Exp)])
 
+type Binder = String
+
+type Tele = [(String,Exp)]
 
 data Exp = Comp Exp Env
          | App Exp Exp
          | Pi Exp Exp
-         | Lam String Exp
+         | Lam Binder Exp
          | Def Exp Def		-- unit substitutions (strings names of definitions)
          | Var Int 		-- generic values
          | Ref String		-- use names
@@ -30,13 +33,13 @@ data Exp = Comp Exp Env
          | PN String Exp        -- primitive notion (typed)
          | Top
          | Undef Integer
-         | EPrim Prim [Exp]  
+         | EPrim Prim [Exp]
   deriving (Eq,Show)
 
 data Prim = PFun Brc
           | PSum Lb
           | PUndef Integer
-  deriving (Eq,Show)            
+  deriving (Eq,Show)
 
 data Env = Empty
          | Pair Env String Val
@@ -44,13 +47,13 @@ data Env = Empty
   deriving (Eq,Show)
 
 upds :: Env -> [(String,Val)] -> Env
-upds [] env = env
-upds ((x,v):xvs) env = upds xvs (Pair env x v)
+upds env []          = env
+upds env ((x,v):xvs) = upds xvs (Pair env x v)
 
 eval :: Exp -> Env -> Val       -- eval is also composition!
-eval (Def env d) s     = eval e (PDef d env)
-eval (App t1 t2)     s = app (eval t1 s) (eval t2 s)
-eval (Pi a b)        s = Pi (eval a s) (eval b s)
+eval (Def e d) s     = eval e (PDef d s)
+eval (App t1 t2) s = app (eval t1 s) (eval t2 s)
+eval (Pi a b)    s = Pi (eval a s) (eval b s)
 eval (Con c ts)    s   = Con c (evals ts s)
 eval (Ref k)       s   = getE k s
 eval U             _   = U
@@ -70,12 +73,12 @@ app f                    u            = App f u
 
 getE :: String -> Env -> Exp
 getE x (Pair _ y u) | x == y       = u
-getE x (Pair s _ _)                = getE (pred k) s
-getE l r@(PDef es r1) = getE l (upds r1 (evals (map fst es) r))
+getE x (Pair s _ _)                = getE x s
+getE x r@(PDef es r1) = getE x (upds r1 (evals (map fst es) r))
 
-addC :: Cont -> [Exp] -> Env -> [(String,Val)] -> Cont
+addC :: Cont -> Tele -> Env -> [(String,Val)] -> Cont
 addC gam _      _  []     = gam
-addC gam (a:as) nu ((x,u):xus) = addC (eval a nu:gam) as (Pair nu x u) us
+addC gam (a:as) nu ((x,u):xus) = addC (eval a nu:gam) as (Pair nu x u) xus
 
 -- An error monad
 type Error a = Either String a
@@ -87,15 +90,15 @@ m =?= s2 = do
   unless (s1 == s2) $ Left ("eqG " ++ show s1 ++ " =/= " ++ show s2)
 
 checkD :: Int -> Env -> Cont -> Def -> Error ()
-checkD k rho gam (as,es) = do
-  (rho1,gam1,l) <- checkTs k rho gam as
-  checks l rho1 gam1 as rho es
+checkD k rho gam (xas,xes) = do
+  (rho1,gam1,l) <- checkTs k rho gam xas
+  checks l rho1 gam1 xas rho xes
 
 checkTs :: Int -> Env -> Cont -> [(String,Exp)] -> Error (Env,Cont,Int)
 checkTs k rho gam []     = return (rho,gam,k)
 checkTs k rho gam ((x,a):xas) = do
   checkT k rho gam a
-  checkTs (k+1) (Pair rho x (Var k)) ((x,eval a rho):gam) as
+  checkTs (k+1) (Pair rho x (Var k)) ((x,eval a rho):gam) xas
 
 checkT :: Int -> Env -> Cont -> Exp -> Error ()
 checkT k rho gam t = case t of
@@ -117,16 +120,16 @@ check k rho gam a t = case (a,t) of
   (U,Sum bs) -> sequence_ [checkTUs k rho gam as | (_,as) <- bs]
   (Pi (Comp (Sum cas) nu) f,Fun _ ces) ->
     if map fst ces == map fst cas
-       then sequence_ [ fix k rho gam as nu f c e
-                      | ((c,e), (_,as)) <- zip ces cas]
+       then sequence_ [ checkBranch k rho gam as nu f c xs e
+                      | ((c,(xs,e)), (_,as)) <- zip ces cas ]
        else fail "case branches does not match the data type"
   (Pi a f,Lam x t)  -> check (k+1) (Pair rho x (Var k)) ((x,a):gam) (app f (Var k)) t
-  (_,Def e (ts,es)) -> -- trace ("checking definition " ++ show str)
+  (_,Def e d@(ts,es)) -> -- trace ("checking definition " ++ show str)
     (do
       checkD k rho gam d
       let rho1 = PDef d rho
       check k rho1 (addC gam (map snd ts) rho (evals es rho1)) a e)
-  (t,Undef n) -> return ()                       
+  (t,Undef n) -> return ()
   _ -> do
     (reifyExp k <$> checkI k rho gam t) =?= reifyExp k a
 
@@ -134,13 +137,15 @@ checkTUs :: Int -> Env -> Cont -> [(String,Exp)] -> Error ()
 checkTUs _ _   _   []     = return ()
 checkTUs k rho gam ((x,a):xas) = do
   check k rho gam U a
-  checkTUs (k+1) (Pair rho x (Var k)) ((x,eval a rho):gam) as
+  checkTUs (k+1) (Pair rho x (Var k)) ((x,eval a rho):gam) xas
 
+--addC :: Cont -> [Exp] -> Env -> [(String,Val)] -> Cont
 -- What does this do?
-fix k rho gam as nu f c e = do
-  let l  = length as
+checkBranch :: Int -> Env -> Cont -> [(Binder,Exp)] -> Env -> Val -> String -> [Binder] -> Exp -> Error ()
+checkBranch k rho gam xas nu f c xs e = do
+  let l  = length xas
   let us = map Var [k..k+l-1]
-  check (k+l) (upds rho us) (addC gam as nu us) (app f (Con c us)) e
+  check (k+l) (upds rho (zip xs us)) (addC gam (map snd xas) nu (zip xs us)) (app f (Con c us)) e
 
 extSG :: String -> Exp -> Error ([Exp], Env)
 extSG c (Comp (Sum cas) r) = case lookup c cas of
@@ -148,10 +153,12 @@ extSG c (Comp (Sum cas) r) = case lookup c cas of
   Nothing -> Left ("extSG " ++ show c)
 extSG c u = Left ("extSG " ++ c ++ " " ++ show u)
 
-checkI :: Int -> Env -> [Exp] -> Exp -> Error Exp
+checkI :: Int -> Env -> Cont -> Exp -> Error Exp
 checkI k rho gam e = case e of
   U -> return U                 -- U : U
-  Ref k   -> return (gam !! k)
+  Ref k   -> case lookup k gam of
+    Just v  -> return v
+    Nothing -> Left $ show k ++ " is not declared!"
   App n m -> do
     c <- checkI k rho gam n
     case c of
@@ -159,24 +166,24 @@ checkI k rho gam e = case e of
         check k rho gam a m
         return (app f (eval m rho))
       _      ->  Left $ show c ++ " is not a product"
-  Def t es str -> trace ("checking definition " ++ show str)
+  Def t d@(xas,xes) -> -- trace ("checking definition " ++ show str)
     (do
-      checkD k rho gam es
-      let rho1 = PDef es rho
-      checkI k rho1 (addC gam (map snd es) rho (evals (map fst es) rho1)) t)
+      checkD k rho gam d
+      let rho1 = PDef d rho
+      checkI k rho1 (addC gam (map snd xas) rho (evals xes rho1)) t)
   PN _ a -> do
     checkT k rho gam a          -- ??
     return (eval a rho)
   _ -> Left ("checkI " ++ show e ++ " in " ++ show rho)
 
 
-checks :: Int -> Env -> [Exp] -> [Exp] -> Env -> [Exp] -> Error ()
+checks :: Int -> Env -> Cont -> [(Binder,Exp)] -> Env -> [(Binder,Exp)] -> Error ()
 checks _ _   _    []    _  []     = return ()
-checks k rho gam (a:as) nu (e:es) = do
+checks k rho gam ((x,a):xas) nu ((_,e) :xes) = do
   -- trace ("checking " ++ show e ++ "\nof type " ++ show a
   --        ++ "\nin " ++ show rho ++ "\n")
   check k rho gam (eval a nu) e
-  checks k rho gam as (Pair nu (eval e rho)) es
+  checks k rho gam xas (Pair nu x (eval e rho)) xes
 checks k rho gam _ _ _ = Left "checks"
 
 
@@ -191,13 +198,15 @@ checkExpInfer t = do
   a <- checkI 0 Empty [] t
   checkExpType t a
 
+genName :: Int -> Binder
+genName n = "##" ++ show n
 
 -- Reification of a value to a term
 reifyExp :: Int -> Exp -> Exp
 reifyExp _ Top                 = Top
 reifyExp _ U                   = U
-reifyExp k (Comp (Lam x t) r)  = Lam $ reifyExp (k+1) (eval t (Pair r x (Var k)))
-reifyExp k (Var l)             = Ref (k-l-1)
+reifyExp k (Comp (Lam x t) r)  = Lam (genName k) $ reifyExp (k+1) (eval t (Pair r x (Var k)))
+reifyExp k (Var l)             = Ref $ genName (k-l-1) -- a new name
 reifyExp k (App u v)           = App (reifyExp k u) (reifyExp k v)
 reifyExp k (Pi a f)            = Pi (reifyExp k a) (reifyExp k f)
 reifyExp k (Con n ts)          = Con n (map (reifyExp k) ts)
@@ -208,7 +217,7 @@ reifyExp k (PN n a)            = PN n (reifyExp k a)
 
 reifyEnv :: Int -> Env -> [Exp]
 reifyEnv _ Empty       = []
-reifyEnv k (Pair r u)  = reifyEnv k r ++ [reifyExp k u] -- TODO: inefficient
+reifyEnv k (Pair r _ u)  = reifyEnv k r ++ [reifyExp k u] -- TODO: inefficient
 reifyEnv k (PDef ts r) = reifyEnv k r
 
 
