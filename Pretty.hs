faeb0b415adb0fb1156996a5d7effda875412011
Simon Huber <hubsim@gmail.com>
2013-12-19 16:47:04 +0100

    pretty printing

---
+++ Pretty.hs
@@ -0,0 +1,28 @@
+-- Common functions used for pretty printing.
+module Pretty where
+
+--------------------------------------------------------------------------------
+-- | Pretty printing combinators. Use the same names as in the pretty library.
+(<+>) :: String -> String -> String
+[] <+> y  = y
+x  <+> [] = x
+x  <+> y  = x ++ " " ++ y
+
+infixl 6 <+>
+
+hcat :: [String] -> String
+hcat []     = []
+hcat [x]    = x
+hcat (x:xs) = x <+> hcat xs
+
+ccat :: [String] -> String
+ccat []     = []
+ccat [x]    = x
+ccat (x:xs) = x <+> ", " <+> ccat xs
+
+parens :: String -> String
+parens p = "(" ++ p ++ ")"
+
+-- Angled brackets, not present in pretty library.
+abrack :: String -> String
+abrack p = "<" ++ p ++ ">"
